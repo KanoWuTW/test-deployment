4b8da34a223980bcfbc87d11d30d322e66ad24eb
Chia-Chen Wu <100563551+KanoWuTW@users.noreply.github.com>
2026-05-08 17:44:59 +0100

    Refactor ALLOWED_HOSTS retrieval from environment

--- GP_Radar/GP_Radar/settings.py
+++ GP_Radar/GP_Radar/settings.py
@@ -30,8 +30,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = []
-allowed_hosts = os.environ.get("ALLOWED_HOSTS", "").split(",")
-ALLOWED_HOSTS = allowed_hosts.split() if allowed_hosts else []
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    ""
+).split(",")
 
 # Application definition
 
