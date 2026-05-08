5fb609d8c51405ced4b22322def89447656fce86
Chia-Chen Wu <100563551+KanoWuTW@users.noreply.github.com>
2026-05-08 21:15:19 +0100

    Switch database backend to PostgreSQL

--- GP_Radar/GP_Radar/settings.py
+++ GP_Radar/GP_Radar/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = "GP_Radar.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": os.environ.get("DB_NAME", "gp_radar"),
-    #     "USER": os.environ.get("DB_USER", "gp_radar_user"),
-    #     "PASSWORD": os.environ.get("DB_PASSWORD", ""),
-    #     "HOST": os.environ.get("DB_HOST", "localhost"),
-    #     "PORT": os.environ.get("DB_PORT", "5432"),
-    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "gp_radar"),
+        "USER": os.environ.get("DB_USER", "gp_radar_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 # Password validation
